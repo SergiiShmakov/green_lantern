35351c4097355d9751ee75cd778bc2845b7e58e2
Sergii Shmakov <sergiishmakov@MBP-Sergii.lan>
2020-06-17 17:21:58 +0300

    django views 1st version

--- lantern/django/django_intro/src/apps/articles/forms.py
+++ lantern/django/django_intro/src/apps/articles/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from django.forms import ModelForm
+
+from src.apps.articles.models import Article
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(required=False)
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ["title", "body"]
+
+
+class ArticleImageForm(forms.Form):
+    image = forms.ImageField(required=True)
--- lantern/django/django_intro/src/apps/articles/urls.py
+++ lantern/django/django_intro/src/apps/articles/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from apps.articles.views import main_page, SearchResultsView
+from src.apps.articles.views import main_page, SearchResultsView, article_json, articles_list_json, ArticlesListView, \
+    ArticlesFormView
 
 app_name = 'articles'
 
 urlpatterns = [
     path('search/', main_page, name='main-page'),
-    path('results/', SearchResultsView.as_view(), name='search-results')
+    path('results/', SearchResultsView.as_view(), name='search-results'),
+    path('json/', article_json, name='json-article-list'),
+    path('json_list/', articles_list_json, name='json-article-list'),
+    path('articles_list/', ArticlesListView.as_view(), name='articles-list'),
+    path('articles_form/', ArticlesFormView.as_view(), name='articles-form'),
+
 ]
